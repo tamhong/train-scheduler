1b1c081e056824b5d5f193fa2c87f4c7e0815f8f
Tam Hong <tamhong.sd@gmail.com>
2018-07-01 22:36:16 -0700

    intial bootstrap layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyC9rFB9WMNaunArxFwDjiKQIJN6dds5TjI",
+    authDomain: "train-scheduler-f5bec.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f5bec.firebaseio.com",
+    projectId: "train-scheduler-f5bec",
+    storageBucket: "",
+    messagingSenderId: "556964055241"
+  };
+  firebase.initializeApp(config);
